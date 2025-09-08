62015f2957e90dfe50442e69f8196c0db8b75a46
Ayomikun <ayoadeosun10@gmail.com>
2025-09-08 13:46:28 +0100

    fix: add event static data

--- components/data.ts
+++ components/data.ts
@@ -1,36 +1,36 @@
 // import { Chapter, Comic, NFTCollectible, Transaction } from "@/lib/types";
 
-// export const eventsData = [
-//   {
-//     id: 1,
-//     src: "/events/comic-con.jpg",
-//     alt: "Comic con",
-//     link: "https://www.straqa.events/nerdworkcomiccon",
-//     date: "September 6, 2025",
-//     title: "Comic Con 2025",
-//     subtitle:
-//       "Experience a day of creativity and connection at our annual community event! ",
-//   },
-//   {
-//     id: 2,
-//     src: "/events/tech-summit.jpg",
-//     alt: "tech summit",
-//     link: "",
-//     date: "May 21, 2025",
-//     title: "Tech Innovation Summit 2025",
-//     subtitle:
-//       "Join industry leaders to explore the latest in technology and innovation.",
-//   },
-//   {
-//     id: 3,
-//     src: "/events/arts-fair.jpg",
-//     alt: "arts fair",
-//     link: "",
-//     date: "June 10, 2025",
-//     title: "Arts & Crafts Fair 2025",
-//     subtitle: "Showcase your talents and discover new art at our annual fair!",
-//   },
-// ];
+export const eventsData = [
+  {
+    id: 1,
+    src: "/events/comic-con.jpg",
+    alt: "Comic con",
+    link: "https://www.straqa.events/nerdworkcomiccon",
+    date: "September 6, 2025",
+    title: "Comic Con 2025",
+    subtitle:
+      "Experience a day of creativity and connection at our annual community event! ",
+  },
+  {
+    id: 2,
+    src: "/events/tech-summit.jpg",
+    alt: "tech summit",
+    link: "",
+    date: "May 21, 2025",
+    title: "Tech Innovation Summit 2025",
+    subtitle:
+      "Join industry leaders to explore the latest in technology and innovation.",
+  },
+  {
+    id: 3,
+    src: "/events/arts-fair.jpg",
+    alt: "arts fair",
+    link: "",
+    date: "June 10, 2025",
+    title: "Arts & Crafts Fair 2025",
+    subtitle: "Showcase your talents and discover new art at our annual fair!",
+  },
+];
 
 // export const comicData: Comic[] = [
 //   {
